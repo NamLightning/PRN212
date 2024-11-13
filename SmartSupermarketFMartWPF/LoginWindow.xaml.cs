5e2cced8744d75e45977a12c11a7f4ba4a8a72d3
nhatkgog <asia.wargaming.vn@gmail.com>
2024-11-13 22:45:35 +0700

    Add Project

---
+++ SmartSupermarketFMartWPF/LoginWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SmartSupermarketFMartWPF
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Button_Click(object sender, RoutedEventArgs e)
+        {
+            string username = txtUsername.Text;
+            string password = txtPassword.Password;
+        }
+
+        private void Reset_Button_Click(object sender, RoutedEventArgs e)
+        {
+            txtUsername.Text = "";
+            txtPassword.Password = "";
+        }
+    }
+}
