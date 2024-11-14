3d026df696e461c3a55a5342155854d3134e4278
NhiDAG <amijeon2019@gmail.com>
2024-11-15 00:11:56 +0700

    update ui login

--- Repositories/obj/Debug/net8.0/Repositories.AssemblyInfo.cs
+++ Repositories/obj/Debug/net8.0/Repositories.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+991b65e026b9eec06b4fcbb023613006618c8bbe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0dd208b47584bb80a13d46c29c97dfb586784e0f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
