0dd208b47584bb80a13d46c29c97dfb586784e0f
NhiDAG <amijeon2019@gmail.com>
2024-11-14 20:42:29 +0700

    update data

--- Repositories/obj/Debug/net8.0/Repositories.AssemblyInfo.cs
+++ Repositories/obj/Debug/net8.0/Repositories.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1d414b0103ebdc341b824bf41c0a666adef5167")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+991b65e026b9eec06b4fcbb023613006618c8bbe")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
